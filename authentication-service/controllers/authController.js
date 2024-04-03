605311d3c49a15f9d5afa09ea52cca3e4eeb4198
Martin Todorov <i481796@fhict.nl>
2024-04-04 00:08:08 +0200

    pipeline testing

--- authentication-service/controllers/authController.js
+++ authentication-service/controllers/authController.js
@@ -7,11 +7,11 @@ passport.use(new LocalStrategy(async (username, password, done) => {
   try {
     const user = await User.findOne({ username });
     if (!user) {
-      return done(null, false, { message: 'Incorrect name1.' });
+      return done(null, false, { message: 'Incorrect username.' });
     }
     const isPasswordValid = await bcrypt.compare(password, user.password);
     if (!isPasswordValid) {
-      return done(null, false, { message: 'Incorrect pass.' });
+      return done(null, false, { message: 'Incorrect password.' });
     }
     return done(null, user);
   } catch (error) {
