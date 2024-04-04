9b7a6b6bf95be65f1a9fd1af874e3ef2743f9ba5
Martin Todorov <i481796@fhict.nl>
2024-04-04 08:12:59 +0200

    sonarcloud integration

--- authentication-service/controllers/authController.js
+++ authentication-service/controllers/authController.js
@@ -7,7 +7,7 @@ passport.use(new LocalStrategy(async (username, password, done) => {
   try {
     const user = await User.findOne({ username });
     if (!user) {
-      return done(null, false, { message: 'Incorrect username1.' });
+      return done(null, false, { message: 'Incorrect username.' });
     }
     const isPasswordValid = await bcrypt.compare(password, user.password);
     if (!isPasswordValid) {
