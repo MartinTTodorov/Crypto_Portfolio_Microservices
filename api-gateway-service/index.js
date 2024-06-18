d82f76e0593bf45da5c731ecc27969d2dd8732a9
MartinTTodorov <martin.todorov@student.fontys.nl>
2024-06-19 00:53:27 +0200

    monitoring and grafana

--- api-gateway-service/index.js
+++ api-gateway-service/index.js
@@ -2,20 +2,27 @@ const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const cors = require('cors');
 
-
-
 const app = express();
 const port = 3000;
+
 app.use(cors());
 
+// Proxy middleware for authentication service
 app.use('/api/auth', createProxyMiddleware({
-  target: `http://localhost:3001/api/auth`,
+  target: 'http://authentication:3001', // Use service name
   changeOrigin: true,
+  pathRewrite: {
+    '': '/api/auth', // Rewrite the path to include /api/auth in the target
+  },
 }));
 
+// Proxy middleware for user management service
 app.use('/api/users', createProxyMiddleware({
-  target: `http://localhost:3002/api/users`,
+  target: 'http://user-management:3002', // Use service name
   changeOrigin: true,
+  pathRewrite: {
+    '^/api/users': '/api/users', // Rewrite the path to include /api/users in the target
+  },
 }));
 
 app.listen(port, () => {
--- authentication-service/server.js
+++ authentication-service/server.js
@@ -1,16 +1,38 @@
 const express = require('express');
 const authRoutes = require('./routes/authRoutes');
 const dotenv = require('dotenv');
-
+const client = require('prom-client');
 
 dotenv.config();
-
 
 const app = express();
 const port = process.env.SERVER_PORT;
 
+// Prometheus metrics
+const collectDefaultMetrics = client.collectDefaultMetrics;
+collectDefaultMetrics({ timeout: 5000 });
+
+const requestCounter = new client.Counter({
+  name: 'http_requests_total',
+  help: 'Total number of HTTP requests',
+  labelNames: ['method', 'route', 'status_code']
+});
+
+// Middleware to count requests
+app.use((req, res, next) => {
+  res.on('finish', () => {
+    requestCounter.inc({ method: req.method, route: req.route ? req.route.path : req.path, status_code: res.statusCode });
+  });
+  next();
+});
+
 app.use(express.json());
 app.use('/api/auth', authRoutes);
+
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', client.register.contentType);
+  res.end(await client.register.metrics());
+});
 
 const server = app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
